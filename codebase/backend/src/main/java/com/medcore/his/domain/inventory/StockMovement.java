af503431aff458547c6bd0d3537ccd7841aa8ae9
Dhaval90750 <thawaredhaval24@gmail.com>
2026-06-29 00:40:03 +0530

    fix(backend): Resolve Entity table mapping conflict for inventory StockMovement

--- codebase/backend/src/main/java/com/medcore/his/domain/inventory/StockMovement.java
+++ codebase/backend/src/main/java/com/medcore/his/domain/inventory/StockMovement.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "stock_movements")
+@Table(name = "inventory_stock_movements")
 @Getter
 @Setter
 public class StockMovement extends BaseEntity {
